c1b6ccbe89230fc5e3aa0d5a3021478923656500
Lhing <thorntan.r@gmail.com>
2024-01-18 15:45:11 +0700

    feat:completed ex5-7 JSON Web Token (JWT) Part 2

--- client/src/utils/jwtInterceptor.js
+++ client/src/utils/jwtInterceptor.js
@@ -7,6 +7,18 @@ function jwtInterceptor() {
     // เมื่อมีการส่ง Request จาก Client ไปหา Server
     // ภายใน Callback Function axios.interceptors.request.use
 
+    // callback function นี้ จะ execute ทุกครั้งที่ axios มีการสร้าง request ไปหา server
+    // ส่วนที่ตรวจสอบว่ามี Token อยู่ใน Local Storage หรือไม่ แล้วจะ convert เป็น Boolean
+    const hasToken = Boolean(window.localStorage.getItem("token"));
+
+    // logic ที่บอกว่า ถ้ามี Token อยู่ใน Local Storage ก็ให้เอา Token นี้ใส่ไปใน Property Authorization ของ Object req.headers
+    if (hasToken) {
+      req.headers = {
+        ...req.headers,
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      };
+    }
+
     return req;
   });
 
@@ -19,7 +31,13 @@ function jwtInterceptor() {
       //  ให้เขียน Logic ในการรองรับเมื่อ Server ได้ Response กลับมาเป็น Error
       // โดยการ Redirect ผู้ใช้งานไปที่หน้า Login และลบ Token ออกจาก Local Storage
       // ภายใน Error Callback Function ของ axios.interceptors.response.use
-
+      if (
+        error.response.status === 401 &&
+        error.response.statusText === "Unauthorized"
+      ) {
+        window.localStorage.removeItem("token"); // ลบ token
+        window.location.replace("/"); // กลับไปหน้า login
+      }
       return Promise.reject(error);
     }
   );
--- server/middlewares/protect.js
+++ server/middlewares/protect.js
@@ -1,3 +1,22 @@
 // 🐨 Todo: Exercise #5
 // สร้าง Middleware ขึ้นมา 1 อันชื่อ Function ว่า `protect`
 // เพื่อเอาไว้ตรวจสอบว่า Client แนบ Token มาใน Header ของ Request หรือไม่
+import jwt from "jsonwebtoken";
+
+export const protect = async (req, res, next) => {
+  const token = req.headers.authorization; // token ที่ client ส่งมา
+  // logic ที่บอกว่าถ้าไม่มี token แนบมากับ header ให้ return ข้อความกลับไปบอก client ว่าให้ส่ง token
+  if (!token || !token.startsWith(`Bearer`)) {
+    return res.status(404).json({ message: "Please send me a JWT token" });
+  }
+  // ใช้ Function jwt.verify จาก Package jsonwebtoken เพื่อตรวจสอบว่า Token นั้นมีความถูกต้องหรือหมดอายุหรือไม่
+  const tokenWithoutBearer = token.split(" ")[1];
+
+  jwt.verify(tokenWithoutBearer, process.env.SECRET_KEY, (err, payload) => {
+    if (err) {
+      return res.json({ message: "JWT token is invalid" });
+    }
+    req.user = payload;
+    next();
+  });
+};
